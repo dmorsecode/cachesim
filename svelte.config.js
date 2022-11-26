c1f307f990aaff2b345ad3df64ea1ca02b85e403
dmorsecode <morsecoding@protonmail.com>
2022-11-26 02:42:45 -0500

    Hopefully done 2.

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			edge: true
+		})
 	},
 	preprocess: [
 		preprocess({
